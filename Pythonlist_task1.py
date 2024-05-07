2adb7a108b8c6a28969bbbc905775e12a057e622
ejones001 <ejonesnavy@gmail.com>
2024-05-07 18:20:00 -0500

    Python List task 1

---
+++ Pythonlist_task1.py
@@ -0,0 +1,3 @@
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93] # Initialize a list called grades
+grades.sort(reverse=True) #sorts the list in descending order
+print("Sorted grades in descending order:", grades) #Prints the list in descending order
