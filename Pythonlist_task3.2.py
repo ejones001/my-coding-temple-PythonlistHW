5d4a6e884542c6fdf33e1c21dd93fe65b5cdd8e6
ejones001 <ejonesnavy@gmail.com>
2024-05-07 19:18:42 -0500

    Python list Reversed temp

---
+++ Pythonlist_task3.2.py
@@ -0,0 +1,5 @@
+temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+
+reversed_temperatures = temperatures[::-1]
+temperatures_5_to_10 = reversed_temperatures[4:10]
+print("Temperatures from the 5th to the 10th day of the reversed list:", temperatures_5_to_10)
